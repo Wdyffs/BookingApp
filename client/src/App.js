b4130b0af11e7ae18c1a78dece8dbeb07bc4199e
Kiryl Sandryhaila <kiruha200146@gmail.com>
2021-12-29 03:41:16 +0300

    feat: branch for fun

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-function App() {
+
+
+export function App() {
   return (
     <div>
       <h1>Hello mern stack!</h1>
+      <div>No matter what you doing!!!</div>
     </div>
   )
 }
