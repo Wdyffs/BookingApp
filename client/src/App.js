8d045ca3d67c7a27df0b17299872edeb852ac0bc
Kiryl Sandryhaila <kiruha200146@gmail.com>
2022-01-06 22:21:34 +0300

    feat: create movie component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Header } from './components/Header/Header'
+import './App.css'
+import { Movies } from './components/Main/Movies/Movies'
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
+      <div className="contentContainer">
+        <Movies />
+        {/* <Theatres /> */}
+      </div>
+      {/* <Footer /> */}
     </div>
   )
 }
