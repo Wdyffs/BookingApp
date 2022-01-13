661d35d75443c1b83766e4c0a1d84567143adf7d
Kiryl Sandryhaila <kiruha200146@gmail.com>
2022-01-13 18:13:21 +0300

    feat: login form work

--- client/src/components/LoginPage/LoginPage.jsx
+++ client/src/components/LoginPage/LoginPage.jsx
@@ -5,26 +5,25 @@ import style from "./LoginPage.module.css";
 export const LoginPage = (props) => {
   return (
     <section className={style.wrapper}>
+      <p className={style.formImage}>
+        <img src="/images/Logo.svg" alt="Login Logo"></img>
+      </p>
       <form className={style.loginForm}>
-        <legend>Registration</legend>
+        <legend className={style.formTitle}>
+          Sign in and start enjoy movies
+        </legend>
         <p className={style.formField}>
-          <label for="email" className={style.fieldName}>
-            Email
-          </label>
           <input
             type="email"
-            placeholder="Type message"
+            placeholder="E-mail"
             id="email"
             className={style.inputField}
           />
         </p>
         <p className={style.formField}>
-          <label for="password" className={style.fieldName}>
-            Password
-          </label>
           <input
             type="password"
-            placeholder="Password must be no less 8 characters"
+            placeholder="Password"
             id="password"
             className={style.inputField}
           />
@@ -34,12 +33,19 @@ export const LoginPage = (props) => {
             Remember me
           </label>
           <input type="checkbox" id="remember" className={style.checkbox} />
+          <span>
+            <Link to="/">Forgot password?</Link>
+          </span>
         </p>
         <button type="submit" className={style.submitButton}>
           <Link to="/" className={style.submitLink}>
-            Submit
+            Sign in
           </Link>
         </button>
+        <p className={style.formFooter}>
+          New to See?
+          <Link to="register">Sign Up</Link>
+        </p>
       </form>
     </section>
   );
